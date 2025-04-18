57aaf76bb2fcc0a0fcb6a704bda47f0c01990685
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-04-18 15:21:50 +0100

    added dotenv to load variables

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
+
 	"github.com/Emmanuella-codes/burnished-microservice/internal/api"
 	"github.com/Emmanuella-codes/burnished-microservice/internal/config"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: Failed to load .env file: %v", err)
+	}
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
